2e72942eaedfba98ae36ec40b2d3ab13e8f15dab
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-10-25 22:32:37 +0600

    passsssssssssssssssssssssssssssssssssssssssssssssssssssssss

---
+++ passssssssssssssssssssssssssssssssssssssss.py
@@ -0,0 +1,15 @@
+num = int(input("Enter number."))
+
+if num % 20 == 0:
+    print("Twist.")
+if num % 15 == 0:
+    pass
+if num % 5 == 0:
+    print("Fizz.")
+if num % 3 == 0:
+    print("Buzz.")
+
+
+
+
+
